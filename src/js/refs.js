4b5d8efd51836c62246b230d9f940e5c92b32f9d
Ivan <110199ws@gmail.com>
2025-03-24 13:44:32 +0200

    fixed problem with requirements

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,7 @@
 const refs = {
   form: document.querySelector('.form-js'),
   input: document.querySelector('.input-js'),
-  list: document.querySelector('.colection-images-js'),
+  list: document.querySelector('.gallery-js'),
   loader: document.querySelector('.loader'),
 }
 
